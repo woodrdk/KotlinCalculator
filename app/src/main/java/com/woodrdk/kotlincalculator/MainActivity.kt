a7fb57790d75bce80f5e041376d145208baa5cf1
Robert Wood Jr <rdrwood@gmail.com>
2019-06-27 17:18:29 -0700

    calculations code

--- app/src/main/java/com/woodrdk/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/woodrdk/kotlincalculator/MainActivity.kt
@@ -81,6 +81,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun performOperation(value: String, operation: String){
-        displayOperation.text = operation
+        if(operand1 == null){
+            operand1 = value.toDouble()
+        }
+        else{
+            operand2 = value.toDouble()
+            if(pendingOperation == "="){
+                pendingOperation = operation
+            }
+            when (pendingOperation){
+                "=" -> operand1 = operand2
+                "/" -> if(operand2 == 0.0){
+                          operand1 = Double.NaN // handles attempt to divide by zero
+                       }
+                       else{
+                          operand1 = operand1!! / operand2
+                       }
+                "*" -> operand1 = operand1!! * operand2
+                "+" -> operand1 = operand1!! + operand2
+                "-" -> operand1 = operand1!! - operand2
+            }
+        }
+        result.setText(operand1.toString())
+        newNumber.setText("")
     }
 }
