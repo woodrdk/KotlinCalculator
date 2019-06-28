210d60ae2e866b0436521cc85e02c1e508ef005f
Robert Wood Jr <rdrwood@gmail.com>
2019-06-27 18:28:36 -0700

    calculations code

--- app/src/main/java/com/woodrdk/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/woodrdk/kotlincalculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     // variables to hold the operands and type of calculation
     private var operand1: Double? = null
-    private var operand2: Double = 0.0
     private var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,10 +64,14 @@ class MainActivity : AppCompatActivity() {
 
         val opListener = View.OnClickListener { v ->
             val op = (v as Button).text.toString()
-            val value = newNumber.text.toString()
-            if (value.isNotEmpty()){
+            try{
+                val value = newNumber.text.toString().toDouble()
                 performOperation(value, op)
             }
+            catch (e: NumberFormatException){
+                newNumber.setText("")
+            }
+
             pendingOperation = op
             displayOperation.text = pendingOperation
         }
@@ -80,26 +84,25 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun performOperation(value: String, operation: String){
+    private fun performOperation(value: Double, operation: String){
         if(operand1 == null){
-            operand1 = value.toDouble()
+            operand1 = value
         }
         else{
-            operand2 = value.toDouble()
             if(pendingOperation == "="){
                 pendingOperation = operation
             }
             when (pendingOperation){
-                "=" -> operand1 = operand2
-                "/" -> if(operand2 == 0.0){
-                          operand1 = Double.NaN // handles attempt to divide by zero
+                "=" -> operand1 = value
+                "/" -> operand1 = if(value == 0.0){
+                          Double.NaN // handles attempt to divide by zero
                        }
                        else{
-                          operand1 = operand1!! / operand2
+                         operand1!! / value
                        }
-                "*" -> operand1 = operand1!! * operand2
-                "+" -> operand1 = operand1!! + operand2
-                "-" -> operand1 = operand1!! - operand2
+                "*" -> operand1 = operand1!! * value
+                "+" -> operand1 = operand1!! + value
+                "-" -> operand1 = operand1!! - value
             }
         }
         result.setText(operand1.toString())
